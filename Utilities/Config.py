b27b4e722a82cc9a684c8bd4f921e7cc37e46a8d
Daniel Adámek <daniel.adamek123@icloud.com>
2023-01-16 23:39:40 +0100

    Config load package

---
+++ Utilities/Config.py
@@ -0,0 +1,21 @@
+from os.path import exists
+
+
+class Config:
+    def __init__(self, path: str):
+        if not exists(path):
+            raise Exception("Config file not found!")
+
+        self.__path: str = path
+        self.__options: dict[str, any] = dict()
+
+        for line in open(path, "r").read().split("\n"):
+            split = line.split(" = ")
+            self.__options[split[0]] = self.__parseValue(split[1])
+        print(self.__options)
+
+    def __parseValue(self, value: str):
+        return int(value) if value.isnumeric() else str(value)
+
+
+config = Config("test.cfg")
