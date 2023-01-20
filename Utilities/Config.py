f45ec3abf2d74de9e452d3f4693fbee178b74863
Daniel Adámek <daniel.adamek123@icloud.com>
2023-01-20 14:40:06 +0100

    config debugged

--- Utilities/Config.py
+++ Utilities/Config.py
@@ -9,12 +9,16 @@ class Config:
         self.__path: str = path
         self.__options: dict[str, any] = dict()
 
-        for line in open(path, "r").read().split("\n"):
-            split = line.split(" = ")
-            self.__options[split[0]] = self.__parseValue(split[1])
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f.read().split("\n"):
+                if len(line) == 0:
+                    continue
+                split = line.split(" = ")
+                self.__options[split[0]] = self.__parseValue(split[1])
 
 
     def __parseValue(self, value: str):
         return int(value) if value.isnumeric() else str(value)
+
     def get(self, value: str) -> any:
         return self.__options.get(value)
