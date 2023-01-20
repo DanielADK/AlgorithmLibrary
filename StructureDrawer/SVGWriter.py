c6c1a17deb9e39afbdb4c19f77e9eec4b77141c7
Daniel Adámek <daniel.adamek123@icloud.com>
2023-01-20 16:04:25 +0100

    SVGWriter working with Node, Edge

--- StructureDrawer/SVGWriter.py
+++ StructureDrawer/SVGWriter.py
@@ -1,15 +1,16 @@
-from Structures import Node, Edge
+from Structures.Node import Node
+from Structures.Edge import Edge
 from Utilities.Config import Config
 
 
 def nodeToSVG(node: (Node, (int, int))) -> str:
     return '<g class="point" transform="translate({x},{y})">' \
-            '<circle></circle>' \
-            '<text class="pointIndex" text-anchor="middle" y="5">' \
-            '{key}' \
-            '</text>' \
-            '</g>' \
-            .format(key=str(node[0].key), x=node[1], y=node[2])
+           '<circle></circle>' \
+           '<text class="pointIndex" text-anchor="middle" y="5">' \
+           '{key}' \
+           '</text>' \
+           '</g>' \
+        .format(key=str(node[0].key), x=node[1], y=node[2])
 
 
 class SVGWriter:
@@ -25,6 +26,25 @@ class SVGWriter:
     def addNode(self, node: Node, x: int, y: int) -> None:
         self.__nodes.append((node, x, y))
 
+    def addEdge(self, a: Node, b: Node) -> None:
+        self.__edges.append(Edge(a,b))
+
+    def edgeToSVG(self, edge: Edge) -> str:
+        # find coods of nodes
+        first: (int, int) = ()
+        second: (int, int) = ()
+        for node in self.__nodes:
+            if edge.first == node[0] or edge.second == node[0]:
+                if first == ():
+                    first = (node[1], node[2])
+                elif second == ():
+                    second = (node[1], node[2])
+            if first != () and second != ():
+                break
+
+        return '<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke="black"/>' \
+            .format(x1=first[0], y1=first[1], x2=second[0], y2=second[1])
+
     def save(self):
         with open(self.__file, "w", encoding="utf-8") as file:
             # Write header SVG tag
@@ -34,9 +54,12 @@ class SVGWriter:
             # Write styles
             file.write(self.__cfg.get("styles")+"\n")
 
-            # Write lines
-
             # Write edges
+            for edge in self.__edges:
+                file.write(self.edgeToSVG(edge))
+                print(self.edgeToSVG(edge))
+
+            # Write nodes
             for node in self.__nodes:
                 file.write(nodeToSVG(node))
                 print(nodeToSVG(node))
--- StructureDrawer/test_SVGWriter.py
+++ StructureDrawer/test_SVGWriter.py
@@ -6,6 +6,9 @@ from Structures.Node import Node
 class TestSVGWriter(TestCase):
     def test_save(self):
         writer = SVGWriter(200, 200, "test.svg", "svgWriterConfig.cfg")
-        writer.addNode(Node[int](1), 80, 80)
-        writer.addNode(Node[str]("ABC"), 150, 80)
+        a = Node[int](1)
+        b = Node[str]("ABC")
+        writer.addNode(a, 80, 80)
+        writer.addNode(b, 150, 120)
+        writer.addEdge(a, b)
         writer.save()
